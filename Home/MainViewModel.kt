69d689579332a04f028c1b44f17ec9da93b5edac
Medan Abebe <medanabebe17@gmail.com>
2025-05-12 17:56:03 +0300

    Add files via upload

---
+++ Home/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.mobileapppro
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MainViewModel : ViewModel() {
+    private val _selectedIndex = MutableStateFlow(0)
+    val selectedIndex: StateFlow<Int> = _selectedIndex
+
+    fun onNavigationItemSelected(index: Int) {
+        _selectedIndex.value = index
+    }
+}
