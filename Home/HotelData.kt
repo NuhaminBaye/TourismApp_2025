980ad35f7bba4d25747b9c4be2e7658ff344d278
Medan Abebe <medanabebe17@gmail.com>
2025-05-12 17:49:06 +0300

    Add files via upload

---
+++ Home/HotelData.kt
@@ -0,0 +1,6 @@
+import android.net.Uri
+
+data class HotelData(
+    val imageUri: Uri?,      // Image selected from local storage
+    val name: String,        // Hotel name (e.g., "Hilton Garden Inn")
+    val region: String,      // Region or location (e.g., "Addis Ababa"))
