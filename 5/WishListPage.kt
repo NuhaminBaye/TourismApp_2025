f794e207fc5fa0508cb0e523b941f03306ebb68f
Meklit Tadie <meklittadie4@gmail.com>
2025-05-12 20:13:39 +0300

    Update WishListPage.kt

--- 5/WishListPage.kt
+++ 5/WishListPage.kt
@@ -1,29 +1,27 @@
-package com.example.mobileapppro
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+
+import com.example.mobileapppro.ExploreViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mobileapppro.HomeCard
+
 
 @Composable
-fun WishList(modifier: Modifier = Modifier){
-    Column(
-        modifier = modifier.fillMaxSize()
-            .background(Color(0xFF197602)),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally)
-    {
-        Text(text = "WishList page",
-            fontSize = 40.sp,
-            fontWeight = FontWeight.SemiBold, color=Color.White)
-    }
+fun WishList(viewModel: ExploreViewModel = viewModel()) {
+    val items by viewModel.items.collectAsState()
+    val likedItems = items.filter { it.isLiked }
 
+    LazyColumn {
+        items(likedItems) { item ->
+            HomeCard(
+                item = item,
+                onLikeToggle = viewModel::toggleLike
+            )
+        }
+    }
 }
+
