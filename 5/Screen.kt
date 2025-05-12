895565e6aa8256020954d1e88b4b62546ec9797e
Meklit Tadie <meklittadie4@gmail.com>
2025-05-12 21:10:23 +0300

    Delete 5 directory

--- 5/Screen.kt
+++
@@ -1,9 +0,0 @@
-
-sealed class Screen(val route: String) {
-    object Login : Screen("login")
-    object SignUp : Screen("signup") 
-    object Home : Screen("home")
-    object EditProfile : Screen("edit_profile")
-    object Settings : Screen("settings")
-    object Feedback : Screen("feedback")
-}
